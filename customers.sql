59f1b44a3e8d3d45ad01fb75931bb9bd3a78b795
Ryan Shadle <46681991+rshadle22@users.noreply.github.com>
2023-01-07 00:49:49 +0000

    commiting back to old cod

--- customers.sql
+++ customers.sql
@@ -3,8 +3,7 @@ SELECT b.customer_id
   , a.email
   , MIN(b.created_at) AS first_order_at
   , COUNT(a.id) AS number_of_orders
-  --Adding a comment here.
-from `analytics-engineers-club.coffee_shop.customers` a
+FROM `analytics-engineers-club.coffee_shop.customers` a
 LEFT JOIN `analytics-engineers-club.coffee_shop.orders` b on a.id = b.customer_id
 GROUP BY 1,2,3
 ORDER BY first_order_at
