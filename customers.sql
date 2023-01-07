1564b08e0ee151e47a89c6b6e09bbf69b5c236ca
Ryan Shadle <46681991+rshadle22@users.noreply.github.com>
2023-01-07 00:09:09 +0000

    add total_value field

--- customers.sql
+++ customers.sql
@@ -3,6 +3,7 @@ SELECT b.customer_id
   , a.email
   , MIN(b.created_at) AS first_order_at
   , COUNT(a.id) AS number_of_orders
+  , SUM(b.total)AS total_order_value
 FROM `analytics-engineers-club.coffee_shop.customers` a
 LEFT JOIN `analytics-engineers-club.coffee_shop.orders` b on a.id = b.customer_id
 GROUP BY 1,2,3
