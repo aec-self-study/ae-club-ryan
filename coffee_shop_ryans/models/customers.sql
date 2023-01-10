4f1ff3a062655855afd0db71d09e885ac8ae720b
Ryan Shadle <46681991+rshadle22@users.noreply.github.com>
2023-01-10 00:30:05 +0000

    Adding another customer model dependency

---
+++ coffee_shop_ryans/models/customers.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+SELECT b.customer_id
+  , a.name
+  , a.email
+  , MIN(b.created_at) AS first_order_at
+  , COUNT(a.id) AS number_of_orders
+FROM `analytics-engineers-club.coffee_shop.customers` a
+LEFT JOIN `analytics-engineers-club.coffee_shop.orders` b on a.id = b.customer_id
+GROUP BY 1,2,3
+ORDER BY first_order_at
+LIMIT 5
