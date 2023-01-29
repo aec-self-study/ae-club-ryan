6102f91663e17b0b78bd3279bf8af1e4f31ff1ad
Ryan Shadle <46681991+rshadle22@users.noreply.github.com>
2023-01-29 20:30:59 +0000

    Changing logic to fix errors

--- coffee_shop_ryans/models/Coffee_shop/Staging/web_tracking/stg_web_tracking_pageviews.sql
+++ coffee_shop_ryans/models/Coffee_shop/Staging/web_tracking/stg_web_tracking_pageviews.sql
@@ -8,13 +8,14 @@ with pageviews as (
     select * from {{ source('web_tracking','pageviews') }}
 )
 , transformed as (
-    select
-        id as event_id,
-        visitor_id,
-        customer_id,
-        device_type,
-        timestamp as event_date,
-        page as page_name
+    select id as event_id
+        , visitor_id
+        , customer_id
+        , page as page_name
+        , device_type
+        , timestamp as event_date
+        , lag(timestamp) over (partition by visitor_id order by timestamp) as last_page_view
     from pageviews
+    order by event_date
 )
 select * from transformed
